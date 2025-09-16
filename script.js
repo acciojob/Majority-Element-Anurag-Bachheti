66b3d4bfc1df582b5aa30db4dcbf43c89b77abfe
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-09-16 13:36:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your code here
+let nums = [1,2,3,4,2,4,1,3,2];
+
+function countMax(nums){
+	let maxCount = 0;
+	let times = [];
+	let maxNum = null;
+
+	for(let num of nums){
+		times[num] = (times[num] || 0)+1;
+
+		if(times[num] > maxCount){
+			times[num] = maxCount;
+			maxNum = num;
+		}
+	}
+	
+	console.log(finalCount);
+}
+countMax(nums);
